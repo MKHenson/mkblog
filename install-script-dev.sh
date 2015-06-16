d2c18ef5a94de61fe5eafb40971751abcf720ae7
MKHenson <mat@webinate.net>
2015-06-16 13:22:01 +0100

    Added a dev install script

---
+++ install-script-dev.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+{ # this ensures the entire script is downloaded #
+
+# Stops the execution of a script if a command or pipeline has an error
+set -e
+
+# Functiom that prints the latest stable version
+version() {
+  echo "0.0.6"
+}
+
+echo "Downloading latest version from github $(version)"
+
+#download latest
+wget https://github.com/MKHenson/mkblog/archive/v$(version).zip
+unzip -o "v$(version).zip"
+
+# Moves the server folder to the current directory
+cp -r mkblog-$(version)/resources/* ./resources
+cp -r mkblog-$(version)/templates/* ./templates
+
+# Removes the temp mkblog folder
+if [ -d "mkblog-$(version)" ]; then
+	rm mkblog-$(version) -R
+fi
+
+# Remove the zip file
+rm "v$(version).zip"
+
+# All done
+echo "MKBlog successfully installed :)"
+exit
+} # this ensures the entire script is downloaded #
