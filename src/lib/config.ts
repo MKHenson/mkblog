95acbd5a43ee75cd4dffeb62cbd24e581e967fae
Mathew Henson <mat@webinate.net>
2016-07-04 23:00:19 +0100

    Updated files & folder structure to kebab-case, added tsconfig and vs code files (WIP). See #6

---
+++ src/lib/config.ts
@@ -0,0 +1,53 @@
+﻿module mkblog
+{
+	'use strict';
+
+	/**
+	* Configures the application
+	*/
+	export class Config
+	{
+        public static $inject = ["$urlRouterProvider", "$stateProvider", "$locationProvider"];
+
+        /**
+		* Creates an instance of the config
+		*/
+        constructor(routeProvider: angular.ui.IUrlRouterProvider, stateProvider: angular.ui.IStateProvider, $locationProvider: angular.ILocationProvider)
+        {
+            // Creates nice URLs
+            $locationProvider.html5Mode(true);
+            $locationProvider.hashPrefix('!');
+            
+            // if the path doesn't match any of the urls you configured
+            // 'otherwise' will take care of routing back to the index
+            routeProvider.otherwise("/");
+            
+            // Create the states
+            stateProvider.state("home", { url: "/", abstract: true, templateUrl: "templates/home.html", controller: "homeCtrl", controllerAs: "controller" });
+
+            stateProvider.state("home.posts", { url: "?author&category&tag&index", templateUrl: "templates/home-posts.html", controller: "homeSubCtrl", controllerAs: "subController" });
+            
+            stateProvider.state("about", { url: "/about", templateUrl: "templates/about.html", controller: "simpleCtrl" });
+            stateProvider.state("contact", { url: "/contact", templateUrl: "templates/contact.html", controller: "contactCtrl", controllerAs: "controller" });
+            stateProvider.state("projects", { url: "/projects?author&category&tag&index", templateUrl: "templates/projects.html", controller: "projectsCtrl", controllerAs: "controller" });
+
+            // Download the post prior to loading this state
+            // then assign the post to the scope
+            stateProvider.state("post", {
+                url: "/post/:slug", templateUrl: "templates/post.html", controller: "postCtrl",
+                resolve: {
+                    post: ["$http", "apiURL", "$stateParams", function ($http: ng.IHttpService, apiURL: string, stateParams)
+                    {
+                        return $http.get<modepress.IGetPost>(`${apiURL}/posts/get-post/${stateParams.slug}`).then(function (posts)
+                        {
+                            if (posts.data.error)
+                                return posts.data;
+
+                            return posts.data.data;
+                        });
+                    }]
+                }
+            });           
+        }
+	}
+}
---
+++ src/lib/meta.ts
@@ -0,0 +1,44 @@
+﻿module mkblog
+{
+    /**
+    * A class bound to the meta tags of the site. You can call this object in controllers with the dependency "meta"
+    */
+    export class Meta
+    {
+        public description: string;
+        public title: string;
+        public brief: string;
+        public smallImage: string;
+        public bigImage: string;
+        public author: string;
+        public website: string;
+        public url: string;
+        public twitterAuthor: string;
+        public twitterSite: string;
+
+        /**
+        * Creates an instance of the meta class
+        */
+        constructor()
+        {
+            this.defaults();
+        }
+
+        /**
+        * Sets the values to their default state
+        */
+        defaults()
+        {
+            this.description = "Mathew Henson's blog page, Dublin based web developer with years of experience in backend and frontend application development";
+            this.title = "Mathew Henson's blog"
+            this.brief = this.description;
+            this.smallImage = _url + "/media/images/mathew-profile.png";
+            this.bigImage = _url + "/media/images/mathew-profile.png";
+            this.author = "Mathew Henson";
+            this.website = "Mathew Henson's Blog";
+            this.url = this.url || "http://mkhenson.com";
+            this.twitterAuthor = "@MathewKHenson";
+            this.twitterSite = "@WebinateNet";
+        }
+    }
+}
